94cee0ad3cca350ea59aa9ead6200fe21cc95095
Jeff Bolsinger <Jbolsinger@me.com>
2023-07-31 15:53:28 -0600

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,5 +1,3 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "dawn-night-42560",
   url: "https://dawn-night-42560.botics.co",
@@ -7,9 +5,9 @@ export const globalOptions = {
   screenOptions: {
     headerShown: false
   }
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
--- screens/welcome/index.js
+++ screens/welcome/index.js
@@ -1,64 +1,29 @@
-import React from "react"
-import {
-  View,
-  Image,
-  Text,
-  ScrollView,
-  SafeAreaView,
-  StyleSheet
-} from "react-native"
+import React from "react";
+import { Text, SafeAreaView, StyleSheet } from "react-native";
 
 const WelcomeScreen = () => {
-  return (
-    <SafeAreaView style={styles.container}>
-      <ScrollView contentContainerStyle={styles.scrollView}>
-        <View style={styles.group} />
-        <View style={styles.group}>
-          <Image style={styles.logo} source={require("./logo.png")} />
-          <Text style={styles.text}>
-            Let's build something amazing together!
-          </Text>
-        </View>
-        <Text style={styles.footer}>Made with ❤️ by Crowdbotics</Text>
-      </ScrollView>
-    </SafeAreaView>
-  )
-}
+  return <SafeAreaView style={styles.container}>
+      
+    <Text style={styles.thDfPfnu}>{"Welcome"}</Text></SafeAreaView>;
+};
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "#F8F8FC",
+    backgroundColor: "#a8bdcb",
     height: "100%"
   },
-  scrollView: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "space-between",
-    padding: 20
-  },
-  group: {
-    alignItems: "center"
-  },
-  logo: {
-    height: 180,
-    width: 180,
-    padding: 40,
-    borderRadius: 30,
-    margin: 40
-  },
-  text: {
+  thDfPfnu: {
+    width: 326,
+    height: 121,
+    lineHeight: 14,
+    fontSize: 50,
+    borderRadius: 0,
+    position: "absolute",
+    left: 17,
     textAlign: "center",
-    fontSize: 28,
-    color: "#828AB0",
-    fontWeight: 700
-  },
-  footer: {
-    textAlign: "center",
-    fontSize: 18,
-    color: "#828AB0",
-    fontWeight: 700,
-    marginBottom: 20
+    top: 72,
+    fontFamily: "Playfair Display",
+    fontWeight: "700"
   }
-})
-
-export default WelcomeScreen
+});
+export default WelcomeScreen;
